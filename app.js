9c06fcd4fdb20eebb64230667acd53b659210bc0
kusl007 <kushal.bansal.1001@gmail.com>
2024-08-23 00:03:19 +0545

    files reading done

--- app.js
+++ app.js
@@ -1,37 +1,35 @@
-const express= require("express")
-const app= express()
-const path = require("path")
-const fs= require("fs")
+const express = require("express");
+const app = express();
+const path = require("path");
+const fs = require("fs");
 
-app.set("view engine","ejs");
-app.use( express.static(path.join(__dirname,"public")))
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => {
+ fs.readdir(`./files`,function(err,files){
+    // console.log(files)
+    res.render("index",{files})
+ })
+});
 
-app.get("/",(req,res)=>{
-res.send("hello")
-})
+app.get("/create", (req, res) => {
+  const now = new Date();
 
-app.get("/create",(req,res)=>{
-    const now = new Date();
-    
-    const day = String(now.getDate()).padStart(2, '0');
-    const month = String(now.getMonth() + 1).padStart(2, '0'); // Months are zero-based
-    const year = now.getFullYear();
-    const fn=(`${day}-${month}-${year}.txt`)
+  const day = String(now.getDate()).padStart(2, "0");
+  const month = String(now.getMonth() + 1).padStart(2, "0"); // Months are zero-based
+  const year = now.getFullYear();
+  const fn = `${day}-${month}-${year}.txt`;
 
-
-fs.writeFile(`./files/${fn}`,"apple" , function(error){
-    if (error){
-        res.send("something went wrong ")
+  fs.writeFile(`./files/${fn}`, "apple", function (error) {
+    if (error) {
+      res.send("something went wrong ");
+    } else {
+      res.send("done");
     }
-    else {
-        res.send("done")
-    }
-})
-})
+  });
+});
 
-
-
-app.listen(3000)
+app.listen(3000);
