34d17d93fe68415b6dba2190cc1e15d696e05c82
kusl007 <kushal.bansal.1001@gmail.com>
2024-08-22 23:44:00 +0545

    server started

---
+++ app.js
@@ -0,0 +1,17 @@
+const express= require("express")
+const app= express()
+const path = require("path")
+
+app.set("view engine","ejs");
+app.use( express.static(path.join(__dirname,"public")))
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+
+
+app.get("/",(req,res)=>{
+res.send("hello")
+})
+
+
+
+app.listen(3000)
